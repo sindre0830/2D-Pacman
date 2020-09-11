d5fdad86af0adf522a76d134bb717be36c124022
Sindre <sindreik@stud.ntnu.no>
2020-09-11 13:02:14 +0200

    Rework -removed most opengl code

--- main.cpp
+++ main.cpp
@@ -7,135 +7,61 @@
  * @date 2020-09-11
  */
 /* libraries */
-#include <cstdlib>
-#include <ios>
 #include <iostream>
-#include <GL/glew.h>
-#include <GLFW/glfw3.h>
-// Error function for GLFW
-void GLFWErrorCallback(int code, const char* description)
-{
-	std::cerr << "Error " << "0x" << std::hex << code << ':' << description << "\n";
-}
-// Error function for OpenGL
-void GLAPIENTRY
-MessageCallback(GLenum source,
-	GLenum type,
-	GLuint id,
-	GLenum severity,
-	GLsizei length,
-	const GLchar* message,
-	const void* userParam)
-{
-	std::cerr << "GL CALLBACK:" << (type == GL_DEBUG_TYPE_ERROR ? "** GL ERROR **" : "") <<
-		"type = 0x" << type <<
-		", severity = 0x" << severity <<
-		", message =" << message << "\n";
-}
+/* global variable */
+int gMap[36][28] = {
+	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1},
+	{1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1},
+	{1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1},
+	{1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1},
+	{1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1},
+	{1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1},
+	{1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1},
+	{1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1},
+	{1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1},
+	{2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+	{1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1},
+	{1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1},
+	{1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1},
+	{1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1},
+	{1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1},
+	{1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1},
+	{1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1},
+	{1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1},
+	{1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1},
+	{1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1},
+	{1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+};
+/* initilizing functions */
+void print();
 /**
  * Main program.
  */
 int main() {
-	glfwSetErrorCallback(GLFWErrorCallback);
-	// Initialization of glfw.
-	if (!glfwInit()) {
-		std::cin.get();
-		return EXIT_FAILURE;
-	}
-	// Create a window
-	glfwWindowHint(GLFW_RESIZABLE, true);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	auto window = glfwCreateWindow(800, 600, "Hello Triangle", nullptr, nullptr);
-	if (window == nullptr) {
-		glfwTerminate();
-		std::cin.get();
-		return EXIT_FAILURE;
-	}
-	//Set the OpenGL context
-	glfwMakeContextCurrent(window);
-	GLenum error = glewInit();
-	if (error != GLEW_OK) {
-		std::cerr << "GLEW intialization failure:" << glewGetErrorString(error) << "\n";
-		std::cin.get();
-		glfwTerminate();
-		return EXIT_FAILURE;
-	}
-	// Enable capture of debug output.
-	glEnable(GL_DEBUG_OUTPUT);
-	glDebugMessageCallback(MessageCallback, 0);
-	// Print OpenGL data
-	std::cout << "Vendor: " << glGetString(GL_VENDOR) << "\n";
-	std::cout << "Renderer: " << glGetString(GL_RENDERER) << "\n";
-	std::cout << "OpenGL version: " << glGetString(GL_VERSION) << "\n";
-	// Clear the background
-	glClearColor(0.5f, 0.0f, 0.0f, 1.0f);
-	// Create a triangle geometry
-	GLfloat triangle[3 * 2] = {
-		-0.5f,	-0.5f,
-		0.5f, 	-0.5f,
-		0.0f, 	0.5f
-	};
-	// Create a vertex array
-	GLuint vertexArrayId;
-	glGenVertexArrays(1, &vertexArrayId);
-	glBindVertexArray(vertexArrayId);
-	// Create a vertex buffer
-	GLuint vertexBufferId;
-	glGenBuffers(1, &vertexBufferId);
-	glBindBuffer(GL_ARRAY_BUFFER, vertexBufferId);
-	// Populate the vertex buffer
-	glBufferData(GL_ARRAY_BUFFER, sizeof(triangle), triangle, GL_STATIC_DRAW);
-	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, nullptr);
-	glEnableVertexAttribArray(0);
-	// Vertex shader code
-	const std::string vertexShaderSrc = R"(
-#version 430 core
-
-layout(location = 0) in vec4 position;
-
-void main()
-{
-gl_Position = position;
+	print();
+	return 0;
 }
-)";
-	// Fragment shader code
-	const std::string fragmentShaderSrc = R"(
-#version 430 core
 
-out vec4 color;
-void main()
-{
-color = vec4(1);
-}
-)";
-	// Compile the vertex shader
-	auto vertexShader = glCreateShader(GL_VERTEX_SHADER);
-	const GLchar* vss = vertexShaderSrc.c_str();
-	glShaderSource(vertexShader, 1, &vss, nullptr);
-	glCompileShader(vertexShader);
-	// Compile the fragment shader
-	auto fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-	const GLchar* fss = fragmentShaderSrc.c_str();
-	glShaderSource(fragmentShader, 1, &fss, nullptr);
-	glCompileShader(fragmentShader);
-	// Create a shader program
-	auto shaderProgram = glCreateProgram();
-	glAttachShader(shaderProgram, vertexShader);
-	glAttachShader(shaderProgram, fragmentShader);
-	glLinkProgram(shaderProgram);
-	glUseProgram(shaderProgram);
-	// Main loop
-	while (!glfwWindowShouldClose(window)) {
-		glfwPollEvents();
-		glClear(GL_COLOR_BUFFER_BIT);
-		glBindBuffer(GL_ARRAY_BUFFER, vertexBufferId);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
-		glfwSwapBuffers(window);
-		// Exit the loop if escape is pressed
-		if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) break;
+void print() {
+	for (int i = 0; i < 36; i++) {
+		for (int j = 0; j < 28; j++) {
+			std::cout << gMap[i][j] << ' ';
+		}
+		std::cout << std::endl;
 	}
-	glfwTerminate();
-	return EXIT_SUCCESS;
 }
