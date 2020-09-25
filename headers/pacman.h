bc5418fa27a17ffe1f975f22f11d4a7f01b1a98b
Sindre <sindreik@stud.ntnu.no>
2020-09-25 15:37:16 +0200

    inital upload

---
+++ headers/pacman.h
@@ -0,0 +1,23 @@
+#ifndef __PACMAN_H
+#define __PACMAN_H
+/* libraries */
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+/**
+ * 'Pacman' class.
+ */
+class Pacman {
+    private:
+        int xPos, yPos;
+        enum direction{right, left, up, down};
+    public:
+        ~Pacman();
+        int collisonCheck();
+        void movUp();
+        void movDown();
+        void movRight();
+        void movLeft();
+        void mov();
+        void draw();
+};
+#endif
---
+++ pacman.cpp
@@ -0,0 +1,12 @@
+/* libraries */
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include "headers/pacman.h"
+Pacman::~Pacman() {}
+int Pacman::collisonCheck() {}
+void Pacman::movUp() {}
+void Pacman::movDown() {}
+void Pacman::movRight() {}
+void Pacman::movLeft() {}
+void Pacman::mov() {}
+void Pacman::draw() {}
